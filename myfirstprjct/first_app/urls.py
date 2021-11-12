dd47abb0f1a1580a87985e835830a6ae1a202e4b
Ahmed Raza <94048578+Ahmed-Raza-4@users.noreply.github.com>
2021-11-11 22:57:24 -0800

    Delete myfirstprjct directory

--- myfirstprjct/first_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from first_app import views
-
-urlpatterns = [
-     path('game/', views.project, name='project'),
-     path('',views.index, name= 'access_record'),
-     path('AccessRecord/',views.accrec, name= 'access_record')
-
-]
